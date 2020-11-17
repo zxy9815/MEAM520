274a17fe1c96ccbd9921e0c54560cd7c7a2f41a3
Griffon McMahon <gmcmahon@purdue.edu>
2020-11-17 18:03:34 -0500

    Implement pathmaking calculations

--- Lab5/potentialFieldPath.py
+++ Lab5/potentialFieldPath.py
@@ -1,5 +1,6 @@
 import numpy as np
 from potentialFieldStep import potentialFieldStep
+from loadmap import loadmap
 
 def potentialFieldPath(map, qStart, qGoal):
     """
@@ -11,3 +12,23 @@ def potentialFieldPath(map, qStart, qGoal):
                         the path. The first row is start and the last row is goal. If no path is found, PATH is a 0x6
                         matrix.
     """
+    isDone = False
+    qCurr = qStart.copy()
+    path = np.array([qStart])
+    
+    while (not(isDone)):
+        print(qCurr)
+        qCurr,isDone = potentialFieldStep(qCurr,map,qGoal)
+        path = np.vstack((path,qCurr))
+    
+    return path
+
+if __name__=='__main__':
+
+    start = np.array([0,  0, 0, 0, 0, 0])
+    goal = np.array([0, 0, 1.1, 0, 0, 0])
+    map = loadmap("maps/map2.txt")
+    path = potentialFieldPath(map,start,goal)
+    
+    print(path)
+        
